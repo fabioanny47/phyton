f305506ca0abef07cb1248777c6f8c056c36fa2c
fabio andres balanta <fabioannypro47@gmail.com>
2021-03-30 21:16:31 -0500

    segundo commit

--- prueba.py
+++ prueba.py
@@ -1,4 +1,8 @@
 def numeros_variables():
-   l = int (input("ingrese valor"))
-   r = int (input("ingrese valor")) 
-   k = int (input("ingrese valor"))
+   l=int (input("ingrese valor"))
+   r=int (input("ingrese valor")) 
+   k=int (input("ingrese valor"))
+   while (l < r):
+      print("hola peluditos")
+
+numeros_variables()      
